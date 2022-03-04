c4981ff981ac493f207b15e432841229691396bb
noobAbishank <nayakabishank1999@gmail.com>
2022-03-04 14:16:33 +0530

    add link in readme

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
           Log In / Register
         </a>
         <div />
-        <a href="/" className="p__opensans">
+        <a href="#login" className="p__opensans">
           Book Table
         </a>
       </div>
